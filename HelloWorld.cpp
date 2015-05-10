aa6b4720e650cb44dd2b3137dd105338017cc871
pyemma <pyemma1991@gmail.com>
2015-05-10 10:24:25 -0700

    Try class

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main() {
 	cout << "Hello World!" << endl;
+	cout << "/*" << endl;
+	cout << "*/" << endl;
+	
+	int value = 0;	
+	while(cin >> value) {
+		cout << value << " ";
+	}
+	cout << endl;
 	return 0;
 }
