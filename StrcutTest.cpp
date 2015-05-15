c032c7465c3650967df235d93f067d4fa28a3997
pyemma <pyemma1991@gmail.com>
2015-05-15 11:32:28 -0700

    Use struct key word to create custom type

---
+++ StrcutTest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+struct MyItem1 {
+	string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0;
+};
+
+int main() {
+	struct MyItem2 {
+		string bookNo;
+		unsigned units_sold = 0;
+		double revenue = 0;
+	};
+}
